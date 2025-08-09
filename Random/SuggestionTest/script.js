17c00aa070d2c8adc43a7e0578554004593c06d3
Jesús Domínguez Ramírez <jesus.dominguez.ramirez855@gmail.com>
2025-08-09 14:15:54 -0600

    Text Suggestion Boceto

---
+++ Random/SuggestionTest/script.js
@@ -0,0 +1,33 @@
+let usersArray = [
+    {
+        username:"Albert",
+        id:""
+    },
+    {
+        username:"",
+        id:""
+    },
+    {
+        username:"",
+        id:""
+    },
+    {
+        username:"",
+        id:""
+    },
+    {
+        username:"",
+        id:""
+    },
+    {
+        username:"",
+        id:""
+    },
+]
+let wrapper = document.createElement('div')
+wrapper.style.cssText=""
+
+const constructor = (id, username) => {
+    
+}
+document.append()
